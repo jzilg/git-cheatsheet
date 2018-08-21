0ccb09be35aacc1f3bc9f67dd77e6cacaded1444
jzilg <jzg@dasburo.com>
2018-08-21 16:56:33 +0200

    creates table of contents in correct html

--- table-of-content.js
+++ table-of-content.js
@@ -1,37 +1,68 @@
 (function() {
-    const subHeadings = document.querySelectorAll('h2, h3')
+    const headings = document.querySelectorAll('h2, h3')
     const toc = document.getElementById('toc')
-    const prevUl = {
-        wasH3: false,
-        ul: null,
+    const parentHeading = {
+        level: undefined,
+        node: undefined,
+    }
+    let subUl
+    let li
+
+    const getHeadingLevel = (heading) => {
+        const headingLevelAsString = heading.tagName.replace('H', '')
+        return parseInt(headingLevelAsString)
     }
 
-    for (let i = 0; i < subHeadings.length; i++) {
-        const heading = subHeadings[i]
-        const id = heading.innerText
-            .toLocaleLowerCase()
-            .replace(/ /g, '-')
+    const getIdFromHeading = heading => heading.innerText
+        .toLocaleLowerCase()
+        .replace(/(,|;|\.|:)/g, '') // remove , ; . :
+        .replace(/ /g, '-') // replace space with -
 
-        const li = document.createElement('li')
+    const getId = (curHeading, parentHeading) => curHeading.level > parentHeading.level
+        ? `${getIdFromHeading(parentHeading.node)}-${getIdFromHeading(curHeading.node)}`
+        : getIdFromHeading(curHeading.node)
+
+    const createLink = (text, id) => {
         const link = document.createElement('a')
         link.href = `#${id}`
-        link.innerText = heading.innerText
+        link.innerText = text
+        return link
+    }
 
-        li.appendChild(link)
+    const addIdToHeading = (heading, id) => {
+        heading.id = id
+    }
 
-        if (heading.tagName === 'H3') {
-            if (!prevUl.wasH3) {
-                prevUl.ul = document.createElement('ul')
+    for (let i = 0; i < headings.length; i++) {
+        const curHeading = {
+            level: getHeadingLevel(headings[i]),
+            node: headings[i]
+        }
+
+        if (!parentHeading.level || curHeading.level <= parentHeading.level) {
+            parentHeading.level = curHeading.level
+            parentHeading.node = curHeading.node
+        }
+
+        const id = getId(curHeading, parentHeading)
+        const link = createLink(curHeading.node.innerText, id)
+
+        if (curHeading.node.tagName === 'H3') {
+            if (!subUl) {
+                subUl = document.createElement('ul')
+                li.appendChild(subUl)
+                toc.appendChild(li)
             }
-            prevUl.ul.appendChild(li)
-            prevUl.wasH3 = true
-
-            toc.appendChild(prevUl.ul)
+            const subLi = document.createElement('li')
+            subLi.appendChild(link)
+            subUl.appendChild(subLi)
         } else {
-            prevUl.wasH3 = false
+            subUl = null
+            li = document.createElement('li')
+            li.appendChild(link)
             toc.appendChild(li)
         }
 
-        heading.id = id
+        addIdToHeading(curHeading.node, id)
     }
 })()
